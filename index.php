9d51fac601f67e18c36ea017e92d768218ecd1f7
jenkfu <56191515+jenkfu@users.noreply.github.com>
2020-08-05 14:42:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Aika est une application" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="application, charge mentale, organisation, famille, mobile" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aika, l'application pour reprendre le contrôle du temps</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <a href="#">
+        <div class="back">></div>
+    </a>
+    <div class="fond"></div>
+    <section class="box">
+        <div class="container">
+            <img src="images/logo.png" alt="" class="logo">
+            <h1 class="aika">AIKA</h1>
+            <h2 class="soutal">Libérez votre esprit</h2>
+        </div>
+    </section>
+    <section class="box">
+        <div class="fonctions">
+            <div> <img src="images/logo.png" alt="" class="aika"> </div>
+            <div class="explications">
+                <p> <strong> Votre esprit n’est jamais au repos ? </strong> </p>
+                <p>
+                    Il vous réveille la nuit pour vous rappeler tout ce à quoi vous devez penser ?<br>
+                    Il vous intime d’écrire des listes à rallonge de tout ce que vous devez faire ?<br>
+                    Il vous met dans un état de stress de peur d’oublier ou de manquer de temps ?<br>
+                </p>
+                <p>
+                    <strong> AIKA est la solution pour libérer votre esprit ! </strong> <br><br>
+
+                    Elle va penser à votre place à toutes ces actions et tâches à accomplir. <br>
+                    Elle va vous accompagner pour réaliser ces actions, voire les effectuer à votre place ! <br>
+                    Elle va vous aider à gagner toujours plus de temps. <br>
+                    Elle va vous encourager en vous récompensant pour les actions réalisées. <br>
+                </p>
+
+            </div>
+        </div>
+        <div class="citation">
+            <img src="images/marie.jpeg" alt="" class="marie">
+            <cite> " Maman de jumeaux avec un emploi très prenant, j’ai constaté que j'étais rapidement dépassée par le nombre de choses auxquelles penser au quotidien. <br>
+Je ne parle pas des tâches ménagères, mais de par exemple penser à organiser les vacances, se rappeler de la visite chez le dentiste, aller voir une amie , ... <br>
+La fameuse charge mentale, des tâches qui tournent dans notre esprit et génèrent un bruit de fond permanent qui nous empêche de profiter des journées, et qui souvent nous réveille la nuit… <br>
+J’ai donc décidé de créer AIKA."<br>
+<p> Marie Lagrange, Fondatrice d’Aika</p>
+         </cite>
+       
+        </div>
+    </section>
+    <section class="box fond2">
+        <h2 class="acc" style="color:white">Notre application</h2>
+        <video controls class="video">
+            <source src="/images/Lancement-MLA.mp4" type="video/mp4">
+        </video>
+<img src="images/imgSchoolBig.png" alt="" class="fille">
+    </section>
+    <section class="box">
+
+        <h2 class="acc">Fonctionnalités</h2>
+        <div class="slideshow-container">
+
+            <!-- Full-width images with number and caption text -->
+            <div class="mySlides fade">
+
+                <div class="contenu">
+                    <img src="images/logo.png" style="width:200px">
+                    <div>
+                        <h3>Création automatique d’actions : </h3>
+                        <p class="fonctext">Grâce à ses algorithmes, AIKA sera capable de prévoir les actions à
+                            planifier dans votre vie à partir de quelques informations renseignées.
+                        
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mySlides fade">
+
+                <div class="contenu">
+                    <img src="images/logo.png" style="width:200px">
+                    <div>
+                        <h3>Gestion des actions : </h3>
+                        <p class="fonctext">En fonction des différents moments de l’année et étapes de la vie, les actions auxquelles vous auriez dû penser vont être générées. Elles apparaîtront ainsi en anticipation, vous évitant d’avoir à y penser.
+                       
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mySlides fade">
+
+                <div class="contenu">
+                    <img src="images/logo.png" style="width:200px">
+                    <div>
+                        <h3>Proposition d’accompagnement : </h3>
+                        <p class="fonctext">Afin de vous aider au maximum et pour vous faire gagner toujours plus de temps, AIKA vous proposera différentes options pour chaque action. Vous pourrez ainsi programmer le bon moment pour vous en occuper, chercher de l’aide sur Internet, ou encore trouver comment faire cela à proximité ...
+                          
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mySlides fade">
+
+                <div class="contenu">
+                    <img src="images/logo.png" style="width:200px">
+                    <div>
+                        <h3> Solution intelligente avec du sur-mesure : </h3>
+                        <p class="fonctext"> Bientôt disponible...
+                           
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mySlides fade">
+
+                <div class="contenu">
+                    <img src="images/logo.png" style="width:200px">
+                    <div>
+                        <h3> Approche qualifiée de la gestion de votre temps : </h3>
+                        <p class="fonctext"> Bientôt disponible...
+                        
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            <div style="text-align:center">
+
+                <!-- The dots/circles -->
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
+                <span class="dot" onclick="currentSlide(4)"></span>
+                <span class="dot" onclick="currentSlide(5)"></span>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="box ">
+        <div class="fond3"></div>
+        <h2 class="soutal">Nos valeurs</h2>
+        <div class="valeurs">
+            <div> <img src="images/img1.png" class="valiens">
+                <legend>DROIT AU BIEN-ÊTRE POUR TOUS</legend>
+            </div>
+            <div> <img src="images/img2.png" class="valiens">
+                <legend>CONSOMMATION RESPONSABLE</legend>
+            </div>
+            <div> <img src="images/img3.png" class="valiens">
+                <legend>VALORISATION DU CIRCUIT-COURT</legend>
+            </div>
+            <div> <img src="images/img4.png" class="valiens">
+                <legend>RESPECT DES DONNEES PERSONNELLES</legend>
+            </div>
+        </div>
+        <h2 class="soutal">Nos soutiens</h2>
+        <div class="soutiens">
+            <div> <img src="images/paris.png" class="valiens">
+                <legend class="none"> BPI FRANCE</legend>
+            </div>
+            <div> <img src="images/bpi.png" class="valiens">
+                <legend class="none">LA MAIRIE DE PARIS</legend>
+            </div>
+            <div> <img src="images/pepiniere.png" class="valiens">
+                <legend class="none">LA PEPINIERE 27</legend>
+            </div>
+            <div> <img src="images/mandarine.png" class="valiens">
+                <legend class="none">MISS MANDARINE</legend>
+            </div>
+        </div>
+    </section>
+
+    <section class="box">
+        <div class="formulaire">
+
+            <div> <img src="images/imgTutoFirst.png" alt="" class="contact"> </div>
+            <?php include('contact.php'); ?>
+            <form id="formu"method="POST" action="index.php#formu">
+                <h2 class="acc">Contactez-nous</h2>
+                <p>Pour une question, une idée à partager, une candidature….</p>
+                <input type="text" name="nom" id="" placeholder="Nom" required>
+                <input type="text" name="entreprise" id="" placeholder="Entreprise">
+                <input type="email" name="email" id="" placeholder="Email" required>
+                <textarea name="message" id="" cols="300" rows="10" placeholder="Votre message" required></textarea>
+                <input type="submit" value="Envoyer">
+                <?php 
+
+if (isset($envoi)) {
+    echo "<p class='envoi'> Votre message a bien été envoyé </p>";
+  }
+
+?>
+            </form>
+
+        </div>
+   
+    </section>
+    <footer>
+    <div class="reseaux">
+        <nav> <a href="politique-de-confidentialite.html"> Politique de confidentialité </a>
+            <a href="cgu.html"> CGU</a>
+            <a href="mentions.html"> Mentions Légales</a>
+        </nav>
+        © AIKA – Tous droits réservés
+      
+        </div>
+        <div class="sociaux">
+            <a href="https://www.facebook.com/aika2020/"><img src="images/facebook.svg" alt=""class="icons"></a>
+            <a href="https://www.instagram.com/take_your_aika/"><img src="images/instagram.svg" alt="" class="icons"></a>
+            <a href="https://www.linkedin.com/company/aika2020/"><img src="images/linkedin.svg" alt="" class="icons"></a>
+        </div>
+       
+    
+    </footer>
+    <script src="caroussel.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+// $(document).ready(function(){
+//   $("a").on('click', function(event) {
+
+//     // Make sure this.hash has a value before overriding default behavior
+//     if (this.hash !== "") {
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
+
+//       // Store hash
+//       var hash = this.hash;
+
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
+//         window.location.hash = hash;
+//       });
+//     } 
+//   });
+// });
+</script>
+</body>
+
+</html>
